629151c5bbc2d8df5357ba914914b7bde260ca11
nenjo tsu <26914800+nenjotsu@users.noreply.github.com>
2026-02-22 10:05:10 +0800

    add findone test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserEntity } from './entities/user.entity';
+import { mock } from 'node:test';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let serivce: UsersService;
+
+  const mockUserService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+
+    findOne: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
+    jest.resetAllMocks();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [{
+        provide: UsersService, useValue: mockUserService}],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    serivce = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    describe('when user is found', () => {
+      const userId = '1';
+      const mockResult = { id: userId, name: 'John Doe', email: 'hVY4W@example.com' };
+
+      beforeEach(() => {
+        serivce.findOne = jest.fn().mockResolvedValue(mockResult);
+      });
+
+      it('should call UsersService.findOne with correct id', async () => {
+        await controller.findOne(userId);
+        expect(serivce.findOne).toHaveBeenCalledWith(userId);
+      });
+
+      it('should return a user with correct params', async () => {
+        const user = { id: 1, name: 'John Doe', email: 'hVY4W@example.com' };
+        const result = await controller.findOne(userId);
+        expect(result).toEqual(mockResult);
+      });
+    });
+
+    describe('when user is not found', () => {
+      const userId = '999';
+
+      beforeEach(() => {
+        serivce.findOne = jest.fn().mockResolvedValue(null);
+      });
+
+      it('should throw a NotFoundException', async () => {
+        await expect(controller.findOne(userId)).rejects.toThrow("User not found");
+      });
+    });
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -18,8 +18,12 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+    if (!user) {
+      throw new NotFoundException("User not found");
+    }
+    return user;
   }
 
   @Patch(':id')
